d1f1003d4cd0e9f9bff239431a48c965ea9202c6
artemiks727 <artemdavalov0609@gmail.com>
2026-09-23 16:31:20 +0300

    commentaries added to circle, rectangle, square, triangle

--- circle.py
+++ circle.py
@@ -2,9 +2,20 @@ import math
 
 
 def area(r):
+    '''
+    Возвращает площадь круга с радиусом r
+        area(r)
+    Параметры
+        r (int): радиус круга
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Возвращает периметр круга с радиусом r
+        perimeter(r)
+    Параметры
+        r (int): радиус круга
+    '''
     return 2 * math.pi * r
-
